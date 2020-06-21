04a4bde5ade07c98947d4f1722d954b0f0c4fd4c
Victor Dallagnol Bento <32740824+da3mons@users.noreply.github.com>
2020-06-21 20:43:07 -0300

    Courte of nodejs from rocketseat

---
+++ www/node-api/src/models/Product.js
@@ -0,0 +1,24 @@
+//  Esquema (schema) para produtos
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate')
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+ProductSchema.plugin(mongoosePaginate);
+
+mongoose.model('Product', ProductSchema);
